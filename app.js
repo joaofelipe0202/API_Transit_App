979ceec44fae26bd4fe0fec89915d01c481c6b8d
Joao Felipe - Johnny <joaobarbo@student.mitt.ca>
2021-01-07 17:33:16 -0300

    create function to request data from transit api

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const apiTokenMapbox = 'pk.eyJ1Ijoiam9hb2ZlbGlwZTAyMDIiLCJhIjoiY2tqbjFhc2p1MDkxczJ5cWxhZDZpbG14NCJ9.HUym9p50_VrPqzQ5JGNGfA';
-const apiKeyTransit = 'cmZaYN5yrwwmepOUIVTd';
+const apiTransitKey = 'cmZaYN5yrwwmepOUIVTd';
 
 const originForm = document.querySelector('.origin-form');
 const destinationForm = document.querySelector('.destination-form');
 const originsList = document.querySelector('.origins');
 const destinationsList = document.querySelector('.destinations');
+const myTrip = document.querySelector('.my-trip');
+
+let originLat, originLong, destinationLat, destinationLong;
 
 const getLocations = (query, resultsList) => {
   return fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${apiTokenMapbox}&limit=10&bbox=-97.325875,49.766204,-96.953987,49.99275`)
@@ -37,6 +40,44 @@ const renderLocations = (placesList, resultsList) => {
   
 }
 
+const getStops = () => {
+  return fetch(`https://api.winnipegtransit.com/v3/trip-planner.json?api-key=${apiTransitKey}&origin=geo/${originCordLat},${originCordLong}&destination=geo/${destinationCordLat},${destinationCordLong}`)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        Promise.reject({response: response.status, response: response.statusText});
+      }
+    })
+}
+
+const planMyTrip = tripData => {
+  let tripInfo = '';
+  tripData.segments.forEach(trip => {
+    if (trip.type === 'walk') {
+      tripInfo = 
+      `
+      <li>
+        <i class="fas fa-walking" aria-hidden="true"></i>${trip.type} for ${trip.times.durations.total} minutes to ${trip.to.stop ===undefined ? 'destination' : `stop #${trip.to.stop.key} - ${trip.to.stop.name}`}
+      </li>`;
+    } else if (trip.type === 'ride') {
+      tripInfo =
+        `
+        <li>
+          <i class="fas fa-bus" aria-hidden="true"></i>${trip.type} the ${trip.route.name === undefined ? trip.route.number : trip.route.name} for ${trip.times.durations.total} minutes.
+        </li>`;
+    } else if (trip.type === 'transfer') {
+      tripInfo =
+      `
+      <li>
+        <i class="fas fa-ticket-alt" aria-hidden="true"></i>${trip.type} from stop #${trip.from.stop.key} - ${trip.from.stop.name} to stop #${trip.from.stop.key} - ${trip.from.stop.name}
+      </li>
+      `;
+    }
+    myTrip.insertAdjacentHTML('afterbegin', tripInfo);
+  })
+}
+
 originForm.addEventListener('submit', e => {
   const originInput = document.querySelector('.origin-value');
   let originInputValue = originInput.value;
@@ -45,7 +86,7 @@ originForm.addEventListener('submit', e => {
     getLocations(originInputValue, originsList);
   }
   
-  originInputValue = '';
+  originInput.value = '';
   e.preventDefault();
 })
 
@@ -57,6 +98,6 @@ destinationForm.addEventListener('submit', e => {
     getLocations(destinationInputValue, destinationsList);
   }
   
-  destinationInputValue = '';
+  destinationInput.value = '';
   e.preventDefault();
 })
